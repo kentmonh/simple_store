8dd3cb2751a73926480cb3b910b802a5cb1f3c61
mnguyen <mnguyen2606@gmail.com>
2020-10-11 23:09:09 +0700

    Last commit
    Last commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,28 @@
-Product.delete_all
+require "csv"
 
-676.times do
-  Product.create(
-    title:          Faker::Commerce.unique.product_name,
-    price:          Faker::Commerce.price,
-    stock_quantity: Faker::Number.non_zero_digit
+# Clearing our the products and categories tables
+Product.destroy_all
+Category.destroy_all
+
+# Loop through the rows of a CSV file
+csv_file = Rails.root.join("db/products.csv")
+
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+products.each do |product|
+  categoty = Category.find_or_create_by(name: product["category"])
+
+  next unless categoty&.valid?
+
+  p = categoty.products.create(
+    title:          product["name"],
+    description:    product["description"],
+    price:          product["price"],
+    stock_quantity: product["stock quantity"]
   )
 end
 
-puts "Created #{Product.count} Products"
+puts "Created #{Category.count} categories"
+puts "Created #{Product.count} products"
